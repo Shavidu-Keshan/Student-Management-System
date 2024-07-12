8c940a7e2532b2088b15dd5cb46f65999f169fd7
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-12 10:19:26 +0530

    i add window

---
+++ StudentManagementSystem.py
@@ -0,0 +1,16 @@
+from tkinter import*
+from tkinter import messagebox
+from PIL import ImageTk,Image
+
+class student:
+    def __init__(self.root):
+        self.root=root
+        self.root.geometry("1000x500+200+100")
+        
+        
+        
+        
+if __name__=="__main__":
+    root=Tk()
+    obj=student(root)
+    root.mainloop()
