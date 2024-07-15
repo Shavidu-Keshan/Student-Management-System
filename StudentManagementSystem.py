00dd5c9d6c0c0f4c8fb9e5a2034f0314be1ea2b6
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-15 23:58:11 +0530

    add adress,contact,email,class label

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -25,14 +25,35 @@ class student:
                       bg='lightgreen', fg='black', anchor='w', padx=20)
         title.place( y=30, relwidth=1, height=50)
         
-        lbl_name = Label(self.root, text="Index No", font=('calibri', 20, 'bold'),
+        lbl_index = Label(self.root, text="Index No", font=('calibri', 20, 'bold'),
                       bg='lightgreen', fg='black',justify=LEFT)
-        lbl_name.place(x=10, y=120, height=30,width=300)
+        lbl_index.place(x=10, y=120, height=30,width=300)
         
         lbl_name = Label(self.root, text="Student Name", font=('calibri', 20, 'bold'),
                       bg='lightgreen', fg='black',justify=LEFT)
         lbl_name.place(x=10, y=160, height=30,width=300)
         
+        
+        lbl_adress = Label(self.root, text="Adress", font=('calibri', 20, 'bold'),
+                      bg='lightgreen', fg='black',justify=LEFT)
+        lbl_adress.place(x=10, y=200, height=30,width=300)
+        
+        lbl_contact = Label(self.root, text="Contact", font=('calibri', 20, 'bold'),
+                      bg='lightgreen', fg='black',justify=LEFT)
+        lbl_contact.place(x=10, y=240, height=30,width=300)
+        
+        
+        lbl_email = Label(self.root, text="Email", font=('calibri', 20, 'bold'),
+                      bg='lightgreen', fg='black',justify=LEFT)
+        lbl_email.place(x=10, y=280, height=30,width=300)
+        
+        lbl_class = Label(self.root, text="Class", font=('calibri', 20, 'bold'),
+                      bg='lightgreen', fg='black',justify=LEFT)
+        lbl_class.place(x=10, y=320, height=30,width=300)
+        
+        #bl_name = Label(self.root, text="Student Name", font=('calibri', 20, 'bold'),
+              #        bg='lightgreen', fg='black',justify=LEFT)
+        #lbl_name.place(x=10, y=160, height=30,width=300)
         
         
 if __name__=="__main__":
