54927105345ab6369b9e7268a7cef610bd60448c
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-17 09:10:00 +0530

    do some changes in label

--- StudentManagementSystem.py
+++ StudentManagementSystem.py
@@ -21,39 +21,48 @@ class student:
         self.lbl_photo_image = Label(self.root, image=self.photo_image, bd=0)
         self.lbl_photo_image.place(x=0, y=0)
         
-        title = Label(self.root, text="Student Management System", font=('calibri', 30, 'bold'),
+        title = Label(self.root, text="Student Management System", font=('times', 30, 'bold'),
                       bg='lightgreen', fg='black', anchor='w', padx=20)
         title.place( y=30, relwidth=1, height=50)
         
-        lbl_index = Label(self.root, text="Index No", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_index.place(x=10, y=120, height=30,width=300)
+        lbl_index = Label(self.root, text="Index No", font=('times', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_index.place(x=10, y=120, height=30,width=200)
         
-        lbl_name = Label(self.root, text="Student Name", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_name.place(x=10, y=160, height=30,width=300)
-        
-        
-        lbl_adress = Label(self.root, text="Adress", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_adress.place(x=10, y=200, height=30,width=300)
-        
-        lbl_contact = Label(self.root, text="Contact", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_contact.place(x=10, y=240, height=30,width=300)
+        lbl_name = Label(self.root, text="Student Name", font=('times', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_name.place(x=10, y=160, height=30,width=200)
         
         
-        lbl_email = Label(self.root, text="Email", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_email.place(x=10, y=280, height=30,width=300)
+        lbl_adress = Label(self.root, text="Adress", font=('times', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_adress.place(x=10, y=200, height=30,width=200)
         
-        lbl_class = Label(self.root, text="Class", font=('calibri', 20, 'bold'),
-                      bg='lightgreen', fg='black',justify=LEFT)
-        lbl_class.place(x=10, y=320, height=30,width=300)
+        lbl_contact = Label(self.root, text="Contact", font=('times', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_contact.place(x=10, y=240, height=30,width=200)
+        
+        
+        lbl_email = Label(self.root, text="Email", font=('times', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_email.place(x=10, y=280, height=30,width=200)
+        
+        lbl_class = Label(self.root, text="Class", font=('timesi', 20, 'bold'),
+                      bg='lightgreen', fg='black',anchor='w')
+        lbl_class.place(x=10, y=320, height=30,width=200)
         
         #bl_name = Label(self.root, text="Student Name", font=('calibri', 20, 'bold'),
               #        bg='lightgreen', fg='black',justify=LEFT)
         #lbl_name.place(x=10, y=160, height=30,width=300)
+        
+        # variables #
+        
+        #self.var_index=StringVar()
+        
+        #entry_index=Entry(self.root,font=('calibri', 20, ),bg='lightgreen', fg='black',justify=RIGHT)
+        #entry_index.place(x=220, y=120, height=30,width=200)
+        
+        #entry_index.focus()
         
         
 if __name__=="__main__":
